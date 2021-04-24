ae2abfbbdfc5341811179645a2b659bcc3e2e4d5
Gil Khais <gil4390@gmail.com>
2021-04-24 17:44:46 +0300

    adding base functions

--- src/PolyMath/Polynomial.java
+++ src/PolyMath/Polynomial.java
@@ -1,4 +1,19 @@
 package PolyMath;
 
 public class Polynomial {
+
+    public static Polynomial build(String s) {
+    }
+
+    public Polynomial add(Polynomial p1) {
+    }
+
+    public Polynomial mul(Polynomial p1) {
+    }
+
+    public Scalar evaluate(Scalar r) {
+    }
+
+    public Polynomial derivative() {
+    }
 }
