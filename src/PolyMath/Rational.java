e055991155f1e53d82c980f9bdf68c4151cd07c3
Gil Khais <gil4390@gmail.com>
2021-04-24 17:07:43 +0300

    start

---
+++ src/PolyMath/Rational.java
@@ -0,0 +1,49 @@
+package PolyMath;
+
+public class Rational implements Scalar{
+
+    @Override
+    public Scalar add(Scalar s) {
+        return null;
+    }
+
+    @Override
+    public Scalar mul(Scalar s) {
+        return null;
+    }
+
+    @Override
+    public Scalar addRational(Rational s) {
+        return null;
+    }
+
+    @Override
+    public Scalar addInteger(Integer s) {
+        return null;
+    }
+
+    @Override
+    public Scalar mulRational(Rational s) {
+        return null;
+    }
+
+    @Override
+    public Scalar mulInteger(Integer s) {
+        return null;
+    }
+
+    @Override
+    public Scalar power(int exponent) {
+        return null;
+    }
+
+    @Override
+    public int sign() {
+        return 0;
+    }
+
+    @Override
+    public Scalar neg() {
+        return null;
+    }
+}
